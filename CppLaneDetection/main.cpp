f812218a7f11c137041d7f7b49aa03fdd3dd18ce
jamesmcgrath88 <jamesmcgrath88@gmail.com>
2025-02-22 07:33:04 +0000

    Add Canny edge detection and ROI masking.

--- CppLaneDetection/main.cpp
+++ CppLaneDetection/main.cpp
@@ -4,9 +4,26 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/core/hal/interface.h>
 #include <string>
 #include <iostream>
-#include "OutputVideo.h""
+#include <vector>
+#include "OutputVideo.h"
+
+static void CreateROIMask(cv::Mat& mask, int width, int height)
+{
+	mask = cv::Mat::zeros(height, width, CV_8U);
+
+	std::vector<cv::Point> fillContSingle;
+	fillContSingle.push_back(cv::Point(0, height));
+	fillContSingle.push_back(cv::Point((width / 2), (height / 2)));
+	fillContSingle.push_back(cv::Point(width, height));
+
+	std::vector<std::vector<cv::Point> > fillContAll;
+	fillContAll.push_back(fillContSingle);
+
+	cv::fillPoly(mask, fillContAll, cv::Scalar(255));
+}
 
 int main(void)
 {
@@ -32,6 +49,12 @@ int main(void)
 
 	cv::Mat frame;
 	cv::Mat grayFrame;
+	cv::Mat cannyFrame;
+	cv::Mat maskedCannyFrame;
+	cv::Mat roiMask;
+	bool initDone = false;
+	int frameCount = 0;
+
 	while (capture.read(frame))
 	{
 		if (frame.empty())
@@ -42,7 +65,20 @@ int main(void)
 
 		cv::cvtColor(frame, grayFrame, cv::COLOR_RGB2GRAY);
 
-		videoOut.WriteFrameToOutputVideo(grayFrame);
+		if (initDone == false)
+		{
+			CreateROIMask(roiMask, grayFrame.cols, grayFrame.rows);
+			initDone = true;
+		}
+
+		cv::Canny(grayFrame, cannyFrame, 100.0f, 200.0f);
+
+		cv::bitwise_and( cannyFrame, roiMask, maskedCannyFrame );
+
+		videoOut.WriteFrameToOutputVideo(maskedCannyFrame);
+		//videoOut.WriteFrameToOutputVideo(roiMask);
+
+		if (++frameCount > 300) { break; }
 	}
 
 	videoOut.SaveOutputVideo();
